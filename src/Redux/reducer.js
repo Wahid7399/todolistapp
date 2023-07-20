0c50219f7f091a55484be5d6e87425382db1d230
Muhammad Wahid <mian.m.wahid@gmail.com>
2023-07-20 13:05:27 +0500

    localstorage added

--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [];
+const initialState = JSON.parse(localStorage.getItem("todos")) ||[];
 
 const addTodoReducer = createSlice({
   name: "todos",
@@ -10,14 +10,33 @@ const addTodoReducer = createSlice({
     //Adding todos
     addTodos: (state, action) => {
       state.push(action.payload);
+      localStorage.setItem("todos", JSON.stringify(state));
       return state;
     },
     //remove todos
     removeTodos: (state, action) => {
+      localStorage.setItem(
+        "todos",
+        JSON.stringify(state.filter((item) => item.id !== action.payload))
+      );
       return state.filter((item) => item.id !== action.payload);
     },
     //update todos
     updateTodos: (state, action) => {
+      localStorage.setItem(
+        "todos",
+        JSON.stringify(
+          state.map((todo) => {
+            if (todo.id === action.payload.id) {
+              return {
+                ...todo,
+                item: action.payload.item,
+              };
+            }
+            return todo;
+          })
+        )
+      );
       return state.map((todo) => {
         if (todo.id === action.payload.id) {
           return {
@@ -30,6 +49,21 @@ const addTodoReducer = createSlice({
     },
     //completed
     completeTodos: (state, action) => {
+      localStorage.setItem(
+        "todos",
+        JSON.stringify(
+          state.map((todo) => {
+            if (todo.id === action.payload) {
+              return {
+                ...todo,
+                completed: true,
+              };
+            }
+            return todo;
+          })
+        )
+      );
+      
       return state.map((todo) => {
         if (todo.id === action.payload) {
           return {
